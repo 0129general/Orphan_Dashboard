fde4f6692283873f6d5625758dce5868813cdcfd
YangHong <wangjack096@gmail.com>
2024-11-14 06:05:09 +0900

    add the build options

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use("/api", userRoutes);
 // orphan system
 app.use("/api/orphanDonors", orphanDonorRoutes);
 // app.use("/api/sponsorFamilies", sponsorFamilyRoutes);
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
